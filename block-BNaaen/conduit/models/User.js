b54f9380123b5f133f9618568d171c05af0443f7
ADITYA NARAYAN SINGH <3daditya@gmail.com>
2021-09-29 12:29:13 +0530

    refactor

--- block-BNaaen/conduit/models/User.js
+++ block-BNaaen/conduit/models/User.js
@@ -1,59 +1,41 @@
-let mongoose = require("mongoose");
-let bcrypt = require("bcryptjs");
-let jwt = require("jsonwebtoken");
-const Profile = require("./Profile");
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+var bcrypt = require("bcrypt");
+var jwt = require("jsonwebtoken");
 
-let Schema = mongoose.Schema;
-
-let userSchema = new Schema({
-  email: { type: String, unique: true, required: true },
-  username: { type: String, unique: true, required: true },
+var userSchema = new Schema({
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  token: String,
-  bio: String,
-  image: { type: String, default: null },
-  profile: { type: mongoose.Types.ObjectId, ref: "Profile" },
-  articles: [{ type: mongoose.Types.ObjectId, ref: "Article" }],
-  comments: [{ type: mongoose.Types.ObjectId, red: "Comment" }],
+  bio: { type: String },
+  image: { type: String },
+  following: [{ type: Schema.Types.ObjectId, ref: "User", unique: true }],
+  followers: [{ type: Schema.Types.ObjectId, ref: "User", unique: true }],
+  // unique for followers not working
 });
 
 userSchema.pre("save", async function (next) {
-  try {
+  if (this.password && this.isModified("password")) {
     this.password = await bcrypt.hash(this.password, 10);
-    let profileData = {
-      username: this.username,
-      bio: this.bio,
-      image: this.image,
-    };
-
-    let profile = await Profile.create(profileData);
-    this.profile = profile.id;
     next();
-  } catch (error) {
-    next(error);
+  } else {
+    next();
   }
 });
 
 userSchema.methods.verifyPassword = async function (password) {
   try {
-    let result = await bcrypt.compare(password, this.password);
+    let result = bcrypt.compare(password, this.password);
     return result;
   } catch (error) {
     return error;
   }
 };
 
-userSchema.methods.createToken = async function (password) {
+userSchema.methods.signToken = async function () {
+  var payload = { userId: this.id, email: this.email };
   try {
-    let profileData = await Profile.findById(this.profile);
-    let payload = {
-      username: profileData.username,
-      bio: profileData.bio,
-      image: profileData.image,
-    };
-
-    let token = await jwt.sign(payload, "thisissecret");
-
+    let token = await jwt.sign(payload, process.env.JWTSECRET);
     return token;
   } catch (error) {
     return error;
@@ -64,21 +46,8 @@ userSchema.methods.userJSON = function (token) {
   return {
     username: this.username,
     email: this.email,
-    bio: this.bio,
-    image: this.image,
     token: token,
   };
 };
 
-userSchema.methods.displayUser = function (id = null) {
-  return {
-    username: this.username,
-    bio: this.bio,
-    image: this.image,
-    following: id ? this.followersList.includes(id) : false,
-  };
-};
-
-let User = mongoose.model("User", userSchema);
-
-module.exports = User;
+module.exports = mongoose.model("User", userSchema);
